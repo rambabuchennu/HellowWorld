2d8e89ee70b7a209a9ca5274dbbb48f12da201ff
rambabu.chennu <rambabu.chennu@gmail.com>
2023-07-31 07:43:23 +0530

    this is first git commit

---
+++ src/ram/com/HellowWorld.java
@@ -0,0 +1,10 @@
+package ram.com;
+
+public class HellowWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is hellow world class!");
+	}
+
+}
